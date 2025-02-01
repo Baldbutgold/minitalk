09b3f9db409eb94c507c6e5dab8a0a03c0a715dc
brr <abdouplayz2003@gmail.com>
2025-02-01 11:07:15 +0100

    found decrypt

---
+++ Minitalk/decrypt.c
@@ -0,0 +1,33 @@
+int	ft_pow(int num, int power)
+{
+	
+	if (power == 0)
+		return (1);
+	if (power == 1)
+		return (num);
+	return (num * ft_pow(num, power - 1));
+}
+char	decrypt_msg(char *byte)
+{
+	int	i;
+	int	j;
+	int	x;
+	char	total;
+	char	*decrypted_string;
+	i = 0;
+	x = 0;
+	while (byte[i])
+	{
+		j = 8;
+		decrypted_string[x] = 0;
+		while(j--)
+		{
+			if (byte[i] == '1')
+				decrypted_string[x] += ft_pow(2, j);
+			i++;
+		}
+		x++;
+	}
+	decrypted_string[5] = 0;
+	printf("%s\n", decrypted_string);
+}
